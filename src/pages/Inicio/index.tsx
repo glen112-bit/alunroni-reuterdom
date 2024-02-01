9859cc38e7a0f78233d2b63141552c5e84d3c76f
Glen112 <escobar.glen@gmail.com>
2024-02-01 01:17:52 -0300

    aluroni-routerdom

---
+++ src/pages/Inicio/index.tsx
@@ -0,0 +1,41 @@
+import cardapio from '../../data/cardapio.json';
+import styles from './Inicio.module.scss';
+import stylesTema from '../../styles/Tema.module.scss';
+import nossaCasa from 'assets/nossa_casa.png';
+import { useNavigate } from 'react-router-dom';
+
+export default function Inicio() {
+  let pratosRecomendados = [...cardapio];
+  pratosRecomendados = pratosRecomendados
+    .sort(() => 0.5 - Math.random())
+    .slice(0, 3);
+
+  const navigate = useNavigate();
+  function redireccionarDetalle(prato: typeof cardapio[0]) {
+    navigate(`/prato/${prato.id}`, {state: {prato}, replace: true});
+  }
+  return (
+    <section>
+      <h3 className={stylesTema.titulo}>Recomendações da cozinha</h3>
+      <div className={styles.recomendados}>
+        {pratosRecomendados.map(item => (
+          <div className={styles.recomendado} key={item.id}>
+            <div className={styles.imagem}>
+              <img src={item.photo} alt={item.title} />
+            </div>
+            <button className={styles.recomendado__botao}
+              onClick={() =>redireccionarDetalle(item)}
+            >ver mas</button>
+          </div>
+        ))}
+      </div>
+      <h3 className={stylesTema.titulo}>Nossa Casa</h3>
+      <div className={styles.nossaCasa}>
+        <img src={nossaCasa} alt="casa do aluroni" />
+        <div className={styles.nossaCasa__endereco}>
+          Rua bla bla bla <br/> <br/> vila no se donde
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/routes.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Inicio from './pages/Inicio';
+import Cardapio from './pages/Cardapio';
+import Menu from './components/Menu';
+import Padron from './components/Padron';
+import Sobre from './pages/Sobre';
+import Footer from './components/Footer';
+import NotFound from './pages/NotFound';
+import Prato from './pages/Prato';
+
+export default function AppRouter() {
+  return (
+    <main className='container'>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path="/" element={ <Padron/> } >
+            <Route index element={<Inicio />} />
+            <Route path="cardapio" element={<Cardapio />} />
+            <Route path='sobre' element={<Sobre />} />
+            <Route path='prato/:id' element={<Prato />} />
+          </Route>
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+        <Footer/>
+      </Router>
+    </main>
+  );
+}
